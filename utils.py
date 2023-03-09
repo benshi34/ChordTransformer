93b5d075a4b80db2cf3d8fdf99dfe681837000bf
benshi <qbshi@princeton.edu>
2023-03-09 00:46:37 -0500

    Init models

---
+++ utils.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import math
+
+class Embedder(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, d_model)
+    def forward(self, x):
+        return self.embed(x)
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len = 80):
+        super().__init__()
+        self.d_model = d_model
+        
+        # positional encoding
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i)/d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
+                
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+        # make positional encodings relatively smaller
+        x = x * math.sqrt(self.d_model)
+        # add constant to embedding
+        seq_len = x.size(1)
+        x = x + Variable(self.pe[:,:seq_len], requires_grad=False).cuda()
+        return x
